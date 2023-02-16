e8eb3c4f803e02911d4999bfcb6d17e28f128935
Hawty1 <49696027+mariusErpenbach@users.noreply.github.com>
2023-02-16 10:21:12 +0100

    Update newTable.js

--- newTable.js
+++ newTable.js
@@ -10,10 +10,12 @@ var con = mysql.createConnection({
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
+    var sql_createTable = "CREATE TABLE products (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), price INT)";
+    var sql_addColumn = "ALTER TABLE products ADD COLUMN targetGroup INT";
+    var sql_dropColumn = "ALTER TABLE customers DROP COLUMN targetGroup";
     // var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
-    con.query(sql, function (err, result) {
+    con.query(sql_dropColumn, function (err, result) {
       if (err) throw err;
-      console.log("Table created");
+      console.log("Table altered");
     });
   });
